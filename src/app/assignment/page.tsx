a6457a0a831c2073a853dcc678399ad4631ecc82
Darren Thammavong <darrent9859@gmail.com>
2023-09-10 17:10:13 -0400

    Add AssignmentContainer component

--- src/app/assignment/page.tsx
+++ src/app/assignment/page.tsx
@@ -1,7 +1,11 @@
+// Components
+import AssignmentContainer from "@/components/AssignmentContainer"
+
 export default function Assignment() {
     return (
-        <div className="bg-[#a5b194] h-screen pt-8">
+        <div className="bg-[#a5b194] h-screen pt-8 p-24">
             <header className="text-center text-6xl font-bold">Upcoming Assignments</header>
+            <AssignmentContainer />
         </div>
     )
 }
---
+++ src/components/AssignmentContainer.tsx
@@ -0,0 +1,3 @@
+export default function AssignmentContainer() {
+    return <div className="aspect-square bg-[#7d8a6a] w-96">Hello</div>
+}
