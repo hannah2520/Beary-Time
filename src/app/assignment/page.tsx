0aae6500f2a11ae876748618f45daa33f66b3ae7
Darren Thammavong <darrent9859@gmail.com>
2023-09-10 16:57:35 -0400

    Add header to assignment page

--- src/app/assignment/page.tsx
+++ src/app/assignment/page.tsx
@@ -1,7 +1,7 @@
 export default function Assignment() {
     return (
-        <div className="bg-[#a5b194] h-screen">
-            <p className="text-3xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero ullam eaque pariatur voluptatem quisquam, vitae reiciendis? Eveniet repudiandae praesentium reprehenderit atque reiciendis, aspernatur, odit, commodi necessitatibus optio iusto delectus dolores.</p>
+        <div className="bg-[#a5b194] h-screen pt-8">
+            <header className="text-center text-6xl font-bold">Upcoming Assignments</header>
         </div>
     )
 }
